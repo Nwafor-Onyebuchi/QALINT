df6af3adcc37ab375ed1633fb80b7b9ad8a00390
Onyebuchi-Nwafor <buchilazarus4@gmail.com>
2019-08-29 13:39:09 -0700

    Updates

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const foot = document.querySelector("#foot01");
+foot.innerHTML =
+  "<p style='color:#ACB3CC'>&copy; " +
+  new Date().getFullYear() +
+  " <a href='' style='text-decoration:none'>Sursa Technology</a>. All right reserved.</p>";
+/*Hide navbar on scroll*/
+let prevScrollPos = window.pageYOffset;
+window.onscroll = () => {
+  let currentScrollPos = window.pageYOffset;
+  if (prevScrollPos > currentScrollPos) {
+    document.querySelector("#nav-header").style.top = "0";
+  } else {
+    document.querySelector("#nav-header").style.top = "-15px";
+  }
+  prevScrollPos = currentScrollPos;
+};
