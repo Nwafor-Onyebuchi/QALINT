36cc4c81d0d08cae568f06a14af7a62579cd305f
Onyebuchi-Nwafor <buchilazarus4@gmail.com>
2019-10-02 16:23:19 +0100

    ui improved

--- dist/js/main.js
+++ dist/js/main.js
@@ -29,7 +29,7 @@ function closeNav() {
 const openChat = $event => {
   $event.preventDefault();
   reply = document.querySelector(".res-in");
-  const username = reply.value;
+  const username = reply.value.toLowerCase();
   const userResponse = document.createElement("p");
   const botResponse = document.createElement("p");
   userResponse.textContent = username;
@@ -42,6 +42,12 @@ const openChat = $event => {
   document.querySelector("#comment-form").reset();
   if (username.indexOf("@") !== -1) {
     botResponse.textContent = "Great! Please enter your GitHub URL?";
+  } else if (username.includes("github") === true) {
+    botResponse.textContent =
+      "Thank you! is there something else you'd like us to know?";
+  } else if (username.includes("no") === true) {
+    botResponse.textContent =
+      "Alright. Please you can exit the chat. Check your inbox for a summary of your response";
   }
 };
 document.querySelector("#btn").addEventListener("click", openChat);
@@ -50,9 +56,9 @@ document.querySelector("#btn").addEventListener("click", openChat);
 const hireChat = $event => {
   $event.preventDefault();
   reply = document.querySelector(".res-in");
-  let username = reply.value;
+  let username = reply.value.toLowerCase();
   const userResponse = document.createElement("p");
-  let botResponse = document.createElement("p");
+  const botResponse = document.createElement("p");
   userResponse.textContent = username;
   botResponse.textContent =
     "Welcome, " + username + ". Nice to meet you! Your email please";
@@ -64,9 +70,15 @@ const hireChat = $event => {
   if (username.indexOf("@") !== -1) {
     companyName = "Great! What's your company name?";
     botResponse.textContent = companyName;
-  } else if (username.includes("github") === true) {
+  } else if (
+    username.includes("ltd") === true ||
+    username.includes("limited") === true
+  ) {
     botResponse.textContent =
-      "Thanks! Is there something else you'd like us to know?";
+      "Nice. Is there something else you'd like to tell us?";
+  } else if (username.includes("no") === true) {
+    botResponse.textContent =
+      "Alright. Please you can exit the chat. Check your inbox for a summary of your response";
   }
 };
 document.querySelector("#btn").addEventListener("click", hireChat);
